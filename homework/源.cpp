2057506245b6389bffe8c37ac0a9136c2a0f31d9
叶志晟 <yzs981130@126.com>
2016-04-01 21:13:15 +0800

    简单的Filter

--- homework/源.cpp
+++ homework/源.cpp
@@ -2,36 +2,36 @@
 #include <string>
 using namespace std;
 template <class T1, class T2>
-void MyForeach(T1 *a, T1 *b, T2 c)
+T1 Filter(T1 a, T1 b, T1 c, T2 d)
 {
-    for (int i = 0; i < b - a; i++)
-        c(*(a + i));
+    int cnt = 0;
+    for (int i = 0;i < b - a; i++)
+        if (d(*(a + i)))
+            c[cnt++] = a[i];
+    return (c + cnt);
 }
-void Print(string s)
+bool LargerThan2(int n)
 {
-    cout << s;
+    return n > 2;
 }
-void Inc(int & n)
+bool LongerThan3(string s)
 {
-    ++n;
+    return s.length() > 3;
 }
-string Array[100];
-int a[100];
+
+string as1[5] = {"Tom","Mike","Jack","Ted","Lucy"};
+string as2[5];
+int  a1[5] = {1,2,3,4,5};
+int a2[5];
 int main()
 {
-    int m, n;
-    while (cin >> m >> n)
-    {
-        for (int i = 0; i < m; ++i)
-            cin >> Array[i];
-        for (int j = 0; j < n; ++j)
-            cin >> a[j];
-        MyForeach(Array, Array + m, Print);
-        cout << endl;
-        MyForeach(a, a + n, Inc);
-        for (int i = 0; i < n; ++i)
-            cout << a[i] << ",";
-        cout << endl;
-    }
+    string * p = Filter(as1, as1 + 5, as2, LongerThan3);
+    for (int i = 0; i < p - as2; ++i)
+        cout << as2[i];
+    cout << endl;
+    int * p2 = Filter(a1, a1 + 5, a2, LargerThan2);
+    for (int i = 0; i < p2 - a2; ++i)
+        cout << a2[i] << ",";
+    system("pause");
     return 0;
 }
