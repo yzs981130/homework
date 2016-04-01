21e13e97c3ac369d96405775210859f461fedb17
叶志晟 <yzs981130@126.com>
2016-04-01 19:05:39 +0800

    这个模板并不难

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
+template <class T>
+class myclass
+{
+public:
+    T *p;
+    int size;
+    myclass(T *_p, int _size)
+    {
+        p = new T[_size + 1];
+        size = _size;
+        for (int i = 0; i < size; i++)
+            *(p + i) = *(_p + i);
+    }
+    ~myclass()
+    {
+        delete[] p;
+    }
+    void Show()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << p[i] << ",";
+        }
+        cout << endl;
+    }
+};
+int a[100];
 int main()
 {
-    
-    system("pasue");
+    char line[100];
+    while (cin >> line)
+    {
+        myclass<char> obj(line, strlen(line));;
+        obj.Show();
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; ++i)
+            cin >> a[i];
+        myclass<int> obj2(a, n);
+        obj2.Show();
+    }
     return 0;
 }
