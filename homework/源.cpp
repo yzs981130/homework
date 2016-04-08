8d13dbc9249fc7893ad0a067eb44e0fa908e46fc
叶志晟 <yzs981130@126.com>
2016-04-08 16:20:22 +0800

    白给的list排序（2）lambda版

--- homework/源.cpp
+++ homework/源.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <algorithm>
 #include <list>
-#include <functional>
 using namespace std;
 int main()
 {
     double a[] = {1.2,3.4,9.8,7.3,2.6};
     list<double> lst(a, a + 5);
-    lst.sort(greater<double>());
+    lst.sort([](double x, double y) {return x > y; });
 
     for (list<double>::iterator i = lst.begin(); i != lst.end(); ++i)
         cout << *i << ",";
