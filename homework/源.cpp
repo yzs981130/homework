584421319be807de827370195ca3b1754f8b3a05
叶志晟 <yzs981130@126.com>
2016-04-09 19:56:05 +0800

    我自己的 ostream_iterator

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,112 +1,44 @@
 #include <iostream>
-#include <iomanip> 
-#include <cstring>
+#include <list>
+#include <string>
 using namespace std;
-template <class T>
-class CArray3D
+
+template <class T1, class T2>
+void Copy(T1 s, T1 e, T2 x)
 {
+    for (; s != e; ++s, ++x)
+        *x = *s;
+}
+
+
+template<class T>
+class myostream_iteraotr
+{
+private:
+    ostream &os;
+    string s;
 public:
-    class CArray2D
+    myostream_iteraotr(ostream _os, string _s):os(_os), s(_s){}
+    void operator ++(){}
+    myostream_iteraotr &operator *() { return *this; }
+    myostream_iteraotr &operator =(const T &val)
     {
-    public:
-        T * poi; 
-        int length, wide;
-        void create(int a, int b)
-        {
-            length = a; wide = b;
-            poi = new T[wide*length];
-        }
-        ~CArray2D()
-        {
-            delete[] poi;
-        }
-        T* operator [](int k)
-        {
-            return poi + k*length;
-        }
-        operator void *()
-        {
-            return (void *)poi;
-        }
-    };
-    int length, wide, high;
-    CArray2D * poi;
-    CArray3D(int a, int b, int c):high(a), wide(b), length(c)
-    {
-        poi = new CArray2D[high];
-        for (int i = 0; i < high; ++i)
-            (poi + i)->create(length, wide);
-    }
-    ~CArray3D()
-    {
-        delete[] poi;
-    }
-    CArray2D& operator [] (int k)
-    {
-        return *(poi + k);
+        os << val << s;
+        return *this;
     }
 };
 
-CArray3D<int> a(3, 4, 5);
-CArray3D<double> b(3, 2, 2);
-void PrintA()
-{
-    for (int i = 0; i < 3; ++i)
-    {
-        cout << "layer " << i << ":" << endl;
-        for (int j = 0; j < 4; ++j)
-        {
-            for (int k = 0; k < 5; ++k)
-                cout << a[i][j][k] << ",";
-            cout << endl;
-        }
-    }
-}
-void PrintB()
-{
-    for (int i = 0; i < 3; ++i)
-    {
-        cout << "layer " << i << ":" << endl;
-        for (int j = 0; j < 2; ++j)
-        {
-            for (int k = 0; k < 2; ++k)
-                cout << b[i][j][k] << ",";
-            cout << endl;
-        }
-    }
-}
 
 int main()
 {
-
-    int No = 0;
-    for (int i = 0; i < 3; ++i)
-    {
-        a[i];
-        for (int j = 0; j < 4; ++j)
-        {
-            a[j][i];
-            for (int k = 0; k < 5; ++k)
-                a[i][j][k] = No++;
-            a[j][i][i];
-        }
-    }
-    PrintA();
-    memset(a[1], -1, 20 * sizeof(int));
-    memset(a[1], -1, 20 * sizeof(int));
-    PrintA();
-    memset(a[1][1], 0, 5 * sizeof(int));
-    PrintA();
-
-    for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 2; ++j)
-            for (int k = 0; k < 2; ++k)
-                b[i][j][k] = 10.0 / (i + j + k + 1);
-    PrintB();
-    int n = a[0][1][2];
-    double f = b[0][1][1];
-    cout << "****" << endl;
-    cout << n << "," << f << endl;
-    system("pause");
+    const int SIZE = 5;
+    int a[SIZE] = {5,21,14,2,3};
+    double b[SIZE] = {1.4, 5.56,3.2,98.3,3.3};
+    list<int> lst(a, a + SIZE);
+    myostream_iteraotr<int> output(cout, ",");
+    Copy(lst.begin(), lst.end(), output);
+    cout << endl;
+    myostream_iteraotr<double> output2(cout, "--");
+    Copy(b, b + SIZE, output2);
     return 0;
 }
