1edb6ec4350650d589c9f8708793e7b43f0b084d
叶志晟 <yzs981130@126.com>
2016-04-14 14:24:55 +0800

    list

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,44 +1,43 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <vector>
 using namespace std;
-
-template <class T1, class T2>
-void Copy(T1 s, T1 e, T2 x)
-{
-    for (; s != e; ++s, ++x)
-        *x = *s;
-}
-
-
-template<class T>
-class myostream_iteraotr
-{
-private:
-    ostream &os;
-    string s;
-public:
-    myostream_iteraotr(ostream _os, string _s):os(_os), s(_s){}
-    void operator ++(){}
-    myostream_iteraotr &operator *() { return *this; }
-    myostream_iteraotr &operator =(const T &val)
-    {
-        os << val << s;
-        return *this;
-    }
-};
-
-
 int main()
 {
-    const int SIZE = 5;
-    int a[SIZE] = {5,21,14,2,3};
-    double b[SIZE] = {1.4, 5.56,3.2,98.3,3.3};
-    list<int> lst(a, a + SIZE);
-    myostream_iteraotr<int> output(cout, ",");
-    Copy(lst.begin(), lst.end(), output);
-    cout << endl;
-    myostream_iteraotr<double> output2(cout, "--");
-    Copy(b, b + SIZE, output2);
+    long n;
+    cin >> n;
+    int id1, id2;
+    list<int> v[10000];
+    while (n--)
+    {
+        string cmd;
+        cin >> cmd >> id1;
+        if (cmd == "add")
+        {
+            cin >> id2;
+            v[id1].push_back(id2);
+        }
+        else if (cmd == "merge")
+        {
+            cin >> id2;
+            v[id1].merge(v[id2]);
+        }
+        else if (cmd == "unique")
+        {
+            v[id1].sort();
+            v[id1].unique();
+        }
+        else if (cmd == "out")
+        {
+            v[id1].sort();
+            for (list<int>::iterator i = v[id1].begin(); i != v[id1].end(); ++i)
+            {
+                cout << *i << ' ';
+            }
+            cout << endl;
+        }
+    }
+    system("pause");
     return 0;
 }
