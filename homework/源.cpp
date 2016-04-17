6b57224b2d7a5bbc5d951635136dd2ae0014cb31
叶志晟 <yzs981130@126.com>
2016-04-17 22:20:06 +0800

    编程填空：数据库内的学生信息

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,95 +1,92 @@
-#include<iostream>
-#include<stdio.h>
-#include<queue>
-#include<iterator>
-#include<algorithm>
-#include<cmath>
+#include <iostream>
+#include <string>
+#include <map>
+#include <iterator>
+#include <algorithm>
+#include <functional>
 using namespace std;
-bool CheckPrimer(int a)
-{
-    int q = sqrt(a);
-    for (int i = 2; i <= q; i++)
-    {
-        if (a % i == 0)
-            return false;
-    }
-    return true;
-}
-int ct(int a)
-{
-    int sum = 0, k;
-    for (int i = 2; i <= sqrt(a); i++)
-    {
-        if (a % i == 0)
-        {
-            k = a / i;
-            if (i != k && CheckPrimer(k))
-                sum++;
-            if (CheckPrimer(i))
-                sum++;
-        }
-    }
-    return sum;
-}
-class cmp1
+template<class Key, class T, class Pred = greater<Key> >
+class MyMultimap:public multimap<Key, T, Pred>
 {
 public:
-    bool operator() (int a, int b)
+    void Set(Key p, T q)
     {
-        int p = ct(a);
-        int q = ct(b);
-        if (p < q)
-            return true;
-        else if (p == q)
-        {
-            if (a < b)
-                return true;
-            else
-                return false;
-        }
-        else
-            return false;
+        for (typename multimap<Key, T, Pred>::iterator i = this->find(p); i != this->end() && i->first == p; ++i)
+            i->second = q;
     }
 };
-class cmp2
+template<class T1, class T2>
+ostream &operator << (ostream &o, pair<T1, T2>&p)
 {
-public:
-    bool operator() (int a, int b)
-    {
-        int p = ct(a);
-        int q = ct(b);
-        if (p < q)
-            return false;
-        else if (p == q)
-        {
-            if (a < b)
-                return false;
-            else
-                return true;
-        }
-        else
-            return true;
-    }
+    o << '(' << p.first << ',' << p.second << ')';
+    return o;
+}
+struct Student
+{
+    string name;
+    int score;
 };
+template <class T>
+void Print(T first, T last)
+{
+    for (; first != last; ++first)
+        cout << *first << ",";
+    cout << endl;
+}
 int main()
 {
-    int num;
-    cin >> num;
-    int n;
-    priority_queue<int, vector<int>, cmp1> pq1;
-    priority_queue<int, vector<int>, cmp2> pq2;
-    for (int i = 0; i < num; i++)
+
+    Student s[] = {{"Tom",80},{"Jack",70},
+    {"Jone",90},{"Tom",70},{"Alice",100}};
+
+    MyMultimap<string, int> mp;
+    for (int i = 0; i < 5; ++i)
+        mp.insert(make_pair(s[i].name, s[i].score));
+    Print(mp.begin(), mp.end()); //按姓名从大到小输出
+
+    mp.Set("Tom", 78); //把所有名为"Tom"的学生的成绩都设置为78
+    Print(mp.begin(), mp.end());
+
+
+
+    MyMultimap<int, string, less<int> > mp2;
+    for (int i = 0; i < 5; ++i)
+        mp2.insert(make_pair(s[i].score, s[i].name));
+
+    Print(mp2.begin(), mp2.end()); //按成绩从小到大输出
+    mp2.Set(70, "Error");          //把所有成绩为70的学生，名字都改为"Error"
+    Print(mp2.begin(), mp2.end());
+    cout << "******" << endl;
+
+    mp.clear();
+
+    string name;
+    string cmd;
+    int score;
+    while (cin >> cmd)
     {
-        for (int j = 0; j < 10; j++)
+        if (cmd == "A")
         {
-            cin >> n;
-            pq1.push(n);
-            pq2.push(n);
+            cin >> name >> score;
+            if (mp.find(name) != mp.end())
+            {
+                cout << "erroe" << endl;
+            }
+            mp.insert(make_pair(name, score));
         }
-        cout << pq1.top() << " ";
-        pq1.pop();
-        cout << pq2.top() << endl;
-        pq2.pop();
+        else if (cmd == "Q")
+        {
+            cin >> name;
+            MyMultimap<string, int>::iterator p = mp.find(name);
+            if (p != mp.end())
+            {
+                cout << p->second << endl;
+            }
+            else
+            {
+                cout << "Not Found" << endl;
+            }
+        }
     }
     system("pause");
     return 0;
