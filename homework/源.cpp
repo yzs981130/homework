c84c0da3bb38b6983fd4e66b316232d7e3a79c17
叶志晟 <yzs981130@126.com>
2016-05-05 17:51:09 +0800

    编程填空：输出指定结果一

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,20 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Number
 {
 public:
     int num;
-    Number(int n = 0): num(n) {}
-    Number operator * (Number b){return num*b.num;}
-    operator int(){return num;}
+    Number(int n): num(n)
+    {}
+    int& value() { return num; }
+    void operator +(Number b) { num = num += b.num; }
 };
-
 int main()
 {
-    Number n1(10), n2(20);
-    Number n3;
-    n3 = n1*n2;
-    cout << int(n3) << endl;
+    Number a(2);
+    Number b = a;
+    cout << a.value() << endl;
+    cout << b.value() << endl;
+    a.value() = 8;
+    cout << a.value() << endl;
+    a + b;
+    cout << a.value() << endl;
     system("pause");
     return 0;
 }
