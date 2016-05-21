e1bd26a3bb53c0c7c3ca30f4c059e7963cfb4b21
叶志晟 <yzs981130@126.com>
2016-05-21 11:06:45 +0800

    UNIMODAL PALINDROMIC DECOMPOSITIONS

--- homework/源.cpp
+++ homework/源.cpp
@@ -2,38 +2,25 @@
 #include <cstdio>
 #include <memory.h>
 #include <cstring>
-#define N 2050
 using namespace std;
-int n;
-char s[N / 2][N];
-void cal(int x, int y, int d)
-{
-    int offset = 1 << (d - 1);
-    if (d == 1)
-    {
-        s[x][y] = s[x + 1][y - 1] = '/';
-        s[x][y + 1] = s[x + 1][y + 2] = '\\';
-        s[x + 1][y] = s[x + 1][y + 1] = '_';
-        return;
-    }
-    cal(x, y, d - 1);
-    cal(x + offset, y - offset, d - 1);
-    cal(x + offset, y + offset, d - 1);
-}
+#define N 252
+long long dp[N][N];
 int main()
 {
-    while (cin >> n && n != 0)
+    int t;
+    for (int i = 1; i < N; i++)
     {
-        memset(s, ' ', sizeof(s));
-        cal(1, (1 << n), n);
-        int k = (1 << n) + 1;
-        for (int i = 1; i <= (1 << n); i++, k++)
-        {
-            for (int j = 1; j <= k; j++)
-                putchar(s[i][j]);
-            printf("\n");
-        }
-        printf("\n");
+        dp[0][i] = 1;
+        for (int j = i / 2 + 1; j <= i; j++)
+            dp[i][j] = 1;
     }
+    for (int i = 2; i < N; i++)
+        for (int j = i / 2; j > 0; j--)
+            dp[i][j] = dp[i - 2 * j][j] + dp[i][j + 1];
+    while (cin >> t && t != 0)
+    {
+        cout << t << ' ' << dp[t][1] << endl;
+    }
+    system("pause");
     return 0;
 }
