f56667aa80c72c861e58c95dc94312534a41bab2
叶志晟 <yzs981130@126.com>
2016-05-21 11:38:11 +0800

    Charm Bracelet(01背包一维)

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 #include <memory.h>
 #include <cstring>
 using namespace std;
-#define N 252
-long long dp[N][N];
+int dp[12882];
+int w[3403];
+int v[3403];
 int main()
 {
-    int t;
-    for (int i = 1; i < N; i++)
-    {
-        dp[0][i] = 1;
-        for (int j = i / 2 + 1; j <= i; j++)
-            dp[i][j] = 1;
-    }
-    for (int i = 2; i < N; i++)
-        for (int j = i / 2; j > 0; j--)
-            dp[i][j] = dp[i - 2 * j][j] + dp[i][j + 1];
-    while (cin >> t && t != 0)
-    {
-        cout << t << ' ' << dp[t][1] << endl;
-    }
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> w[i] >> v[i];
+    for (int i = 1; i <= n; i++)
+        for (int j = m; j >= w[i]; j--)
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+    cout << dp[m] << endl;
     system("pause");
     return 0;
 }
