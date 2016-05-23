b7dae02b21c24a55554e04f487ea3dc764c9e0b8
叶志晟 <yzs981130@126.com>
2016-05-23 17:41:13 +0800

    阶乘

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,29 +1,49 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <memory.h>
-#include <cstring>
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdio>
+#include<stdlib.h>
+#include<string.h>
+#define QUOTIETY 4
 using namespace std;
-int a[26], b[26];
+int cnt = 1;
+void multiply(const int index, int *result)
+{
+    int product = 0;
+    int carry = 0;
+    int remainder = 0;
+    int i = 0;
+    for (i = 0; i < cnt; ++i)
+    {
+        product = result[i] * index + carry;
+        carry = product / 10;
+        remainder = product % 10;
+        result[i] = remainder;
+    }
+    if (carry != 0)
+    {
+        while (carry / 10 != 0)
+        {
+            result[cnt] = carry % 10;
+            carry /= 10;
+            ++cnt;
+        }
+        result[cnt++] = carry;
+    }
+}
 int main()
 {
-    string s1, s2;
-    cin >> s1 >> s2;
-    for (int i = 0; i < s1.length(); i++)
-        a[s1[i] - 'A']++;
-    for (int i = 0; i < s2.length(); i++)
-        b[s2[i] - 'A']++;
-    sort(a, a + 26);
-    sort(b, b + 26);
-    for(int i = 0; i < 26; i++)
-        if (a[i] != b[i])
-        {
-            cout << "NO" << endl;
-            return 0;
-        }
-    cout << "YES" << endl;
+    int index = 0;
+    int input = 0;
+    int *result = NULL;
+    scanf("%d", &input);
+    result = new int [sizeof(int) * input * QUOTIETY];
+    memset(result, 0, sizeof(int) * input * QUOTIETY);
+    result[0] = 1;
+    for (index = 1; index <= input; ++index)
+        multiply(index, result);
+    for (index = cnt - 1; index >= 0L; --index)
+        printf("%d", result[index]);
+    delete []result;
     system("pause");
     return 0;
 }
