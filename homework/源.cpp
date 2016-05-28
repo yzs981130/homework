6051fd41ba6ab743deff6355c5154338e8a950fd
叶志晟 <yzs981130@126.com>
2016-05-28 16:12:24 +0800

    迷宫问题

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,43 +1,71 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<cstdio>
+#include<climits>
 #include<stdlib.h>
 #include<string.h>
-#define MAX 10
-char m[MAX][MAX];
-int n, k;
-int cnt;
-bool vis[MAX];
+#define MAX 5
 using namespace std;
-void dfs(int row, int num)
+int dir[4][2] = {{0, 1},{0, -1},{1, 0},{-1, 0}};
+int m[MAX][MAX], vis[MAX][MAX];
+struct node
 {
-    if (num == k)
+    int x;
+    int y;
+    int pre;
+}que[1000];
+int head = 0, tail = 1;
+void print(int i)
+{
+    if (que[i].pre != -1)
     {
-        cnt++;
-        return;
+        print(que[i].pre);
+        cout << '(' << que[i].x << ", " << que[i].y << ')' << endl;
     }
-    for(int i = row; i < n; i++)
-        for(int j = 0; j < n; j++)
-            if (m[i][j] == '#' && !vis[j])
+}
+void bfs(int x, int y)
+{
+    que[head].x = x;
+    que[head].y = y;
+    que[head].pre = -1;
+    bool flag = false;
+    while (head < tail)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            int tx = que[head].x + dir[i][0];
+            int ty = que[head].y + dir[i][1];
+            if (tx < 0 || tx >= MAX || ty < 0 || ty >= MAX || vis[tx][ty] == 1 || m[tx][ty] == 1)
+                continue;
+            else
             {
-                vis[j] = true;
-                dfs(i + 1, num + 1);
-                vis[j] = false;
+                vis[x][y] = 1;
+                que[tail].x = tx;
+                que[tail].y = ty;
+                que[tail].pre = head;
+                tail++;
             }
+            if (tx == MAX - 1 && ty == MAX - 1)
+            {
+                print(head);
+                flag = true;
+            }
+        }
+        if (flag)
+            break;
+        head++;
+    }
 }
 int main()
 {
-    while (cin >> n >> k && (n != -1))
-    {
-        cnt = 0;
-        memset(vis, 0, sizeof(vis));
-        memset(m, 0, sizeof(m));
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                cin >> m[i][j];
-        dfs(0, 0);
-        cout << cnt << endl;
-    }
+    for (int i = 0; i < MAX; i++)
+        for (int j = 0; j < MAX; j++)
+            cin >> m[i][j];
+    memset(vis, 0, sizeof(vis));
+    vis[0][0] = 1;
+    cout << "(0, 0)" << endl;
+    bfs(0, 0);
+    cout << "(4, 4)" << endl;
     system("pause");
     return 0;
 }
