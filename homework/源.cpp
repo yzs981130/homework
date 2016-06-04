ab22e60ca9a6e310461160c5def16c515ff03cf8
叶志晟 <yzs981130@126.com>
2016-06-04 21:38:42 +0800

    采药

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,63 +1,16 @@
-#include <iostream>
-#include <queue>
-#include <cstring>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
 using namespace std;
-#define MAX 1000001
-int s, t, step[MAX];
-bool vis[MAX];
-int bfs(int s, int t)
-{
-    queue<int>q;
-    q.push(s);
-    vis[s] = true;
-    while (!q.empty())
-    {
-        int head = q.front();
-        q.pop();
-        vis[head] = true;
-        if (head == t)
-            break;
-        if (head - 1 >= 0 && !vis[head - 1])
-        {
-            vis[head - 1] = true;
-            q.push(head - 1);
-            step[head - 1] = step[head] + 1;
-        }
-        if (head + 1 < MAX && !vis[head + 1])
-        {
-            vis[head + 1] = true;
-            q.push(head + 1);
-            step[head + 1] = step[head] + 1;
-        }
-        if (2 * head < MAX && !vis[2 * head])
-        {
-            vis[2 * head] = true;
-            q.push(2 * head);
-            step[2 * head] = step[head] + 1;
-        }
-        if (head % 2 == 0 && !vis[head / 2])
-        {
-            vis[head / 2] = true;
-            q.push(head / 2);
-            step[head / 2] = step[head] + 1;
-        }
-    }
-    return step[t];
-}
+int totalTime, number, v[101], t[101], maxValue[1005];
 int main()
 {
-    int n;
-    cin >> n;
-    while (n--)
-    {
-        cin >> s >> t;
-        memset(vis, 0, sizeof(vis));
-        memset(step, 0, sizeof(step));
-        int a = bfs(s, t);
-        memset(vis, 0, sizeof(vis));
-        memset(step, 0, sizeof(step));
-        int b = bfs(t, s);
-        cout << a + b << endl;
-    }
+    cin >> totalTime >> number;
+    for (int i = 0; i < number; i++)
+        cin >> t[i] >> v[i];
+    for (int i = 0; i < number; i++)
+        for (int j = 1000; j >= t[i]; j--)
+            maxValue[j] = max(maxValue[j], maxValue[j - t[i]] + v[i]);
+    cout << maxValue[totalTime] << endl;
     return 0;
 }
