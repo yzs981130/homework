0ca4de12ac6c56aaafa0b1f234bd1e5b0d948f59
叶志晟 <yzs981130@126.com>
2016-06-04 22:04:44 +0800

    The Troublesome Frog

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,16 +1,72 @@
 #include<iostream>
+#include<cstdio>
 #include<algorithm>
 #include<cstring>
+#define max(a,b) ((a)>(b)?(a):(b))
 using namespace std;
-int totalTime, number, v[101], t[101], maxValue[1005];
+const int MAX = 5005;
+
+struct point
+{
+    int r, c;
+}fro[MAX];
+int row, col;
+bool map[MAX][MAX];
+
+bool cmp(point a, point b)
+{
+    if (a.c == b.c) return a.r < b.r;
+    return a.c < b.c;
+}
+
+bool inMap(int r, int c)
+{
+    if (r > 0 && r <= row && c > 0 && c <= col) return true;
+    return false;
+}
+
+int getSum(int r, int c, int dr, int dc)
+{
+    int sum = 0;
+    while (inMap(r, c))
+    {
+        if (!map[r][c]) return 0;
+        r += dr;
+        c += dc;
+        sum++;
+    }
+    return sum;
+}
+
 int main()
 {
-    cin >> totalTime >> number;
-    for (int i = 0; i < number; i++)
-        cin >> t[i] >> v[i];
-    for (int i = 0; i < number; i++)
-        for (int j = 1000; j >= t[i]; j--)
-            maxValue[j] = max(maxValue[j], maxValue[j - t[i]] + v[i]);
-    cout << maxValue[totalTime] << endl;
+    int n, i, j;
+    int r, c, dr, dc;
+    while (scanf("%d%d", &row, &col) != EOF)
+    {
+        memset(map, 0, sizeof(map));
+        scanf("%d", &n);
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d%d", &r, &c);
+            map[r][c] = 1;
+            fro[i].r = r;
+            fro[i].c = c;
+        }
+        sort(fro, fro + n, cmp);
+        int ans = 2;
+        for (i = 0; i < n - 1; i++)
+            for (j = i + 1; j < n; j++)
+            {
+                dr = fro[j].r - fro[i].r;
+                dc = fro[j].c - fro[i].c;
+                if (fro[i].c + ans*dc > col) break;
+                if (inMap(fro[i].r - dr, fro[i].c - dc)) continue;
+                if (!inMap(fro[i].r + ans*dr, fro[i].c + ans*dc)) continue;
+                ans = max(ans, getSum(fro[i].r, fro[i].c, dr, dc));
+            }
+        if (ans < 3) printf("0\n");
+        else printf("%d\n", ans);
+    }
     return 0;
 }
