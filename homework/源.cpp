f8642e6799a05bd444990af5bc2979e9ecf70b96
叶志晟 <yzs981130@126.com>
2016-06-24 08:36:13 +0800

    1915:Knight Moves

--- homework/源.cpp
+++ homework/源.cpp
@@ -1,49 +1,21 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
 #include <queue>
 using namespace std;
-#define MAX 202
-char m[MAX][MAX];
-bool vis[MAX][MAX];
-int step[MAX][MAX];
-int r, c;
-int dir[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-struct point
+struct Pos
 {
     int x;
     int y;
-    point(int _x, int _y):x(_x), y(_y){}
+    int step;
+    Pos(int _x = 0, int _y = 0):x(_x), y(_y){}
 };
-bool is_valid(int x, int y)
+int n, dir[8][2] = {{1, 2},{2, 1},{2, -1},{1, -2},{-1, -2},{-2, -1},{-2, 1},{-1, 2}};
+bool vis[310][310];
+bool valid(int x, int y)
 {
-    if (vis[x][y] == true || x >= r || x < 0 || y >= c || y < 0 || m[x][y] == '#')
-        return false;
-    return true;
-}
-int bfs(int b_x, int b_y, int e_x, int e_y)
-{
-    point p(b_x, b_y);
-    queue<point> q;
-    q.push(p);
-    vis[b_x][b_y] = true;
-    while (!q.empty())
-    {
-        point c = q.front();
-        q.pop();
-        if (c.x == e_x && c.y == e_y)
-            return step[e_x][e_y];
-        for (int i = 0; i < 4; i++)
-        {
-            if(!is_valid(c.x + dir[i][0], c.y + dir[i][1]))
-                continue;
-            point d(c.x + dir[i][0], c.y + dir[i][1]);
-            vis[c.x + dir[i][0]][c.y + dir[i][1]] = true;
-            q.push(d);
-            step[c.x + dir[i][0]][c.y + dir[i][1]] = step[c.x][c.y] + 1;
-        }
-    }
-    return -1;
+    if (x >= 0 && x < n && y >= 0 && y < n)
+        return true;
+    return false;
 }
 int main()
 {
@@ -51,31 +23,35 @@ int main()
     cin >> t;
     while (t--)
     {
-        memset(m, 0, sizeof(m));
-        memset(step, 0, sizeof(step));
+        cin >> n;
+        Pos st, en;
         memset(vis, 0, sizeof(vis));
-        cin >> r >> c;
-        int b_x, b_y, e_x, e_y;
-        for (int i = 0; i < r; i++)
-            for (int j = 0; j < c; j++)
+        cin >> st.x >> st.y >> en.x >> en.y;
+        vis[st.x][st.y] = true;
+        st.step = 0;
+        queue<Pos> q;
+        q.push(st);
+        while (!q.empty())
+        {
+            Pos tmp = q.front();
+            q.pop();
+            if (tmp.x == en.x && tmp.y == en.y)
             {
-                cin >> m[i][j];
-                if (m[i][j] == 'S')
+                cout << tmp.step << endl;
+                break;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                int tx = tmp.x + dir[i][0], ty = tmp.y + dir[i][1];
+                if (!vis[tx][ty] && valid(tx, ty))
                 {
-                    b_x = i;
-                    b_y = j;
-                }
-                if (m[i][j] == 'E')
-                {
-                    e_x = i;
-                    e_y = j;
+                    Pos m(tx, ty);
+                    m.step = tmp.step + 1;
+                    q.push(m);
+                    vis[tx][ty] = true;
                 }
             }
-        int t = bfs(b_x, b_y, e_x, e_y);
-        if (t == -1)
-            cout << "oop!" << endl;
-        else
-            cout << t << endl;
+        }
     }
     system("pause");
     return 0;
